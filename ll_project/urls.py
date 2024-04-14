b84bfdb1a548b986b5f6b7991e8344d953ac72a6
Kirill777-web <ki.lazutin@gmail.com>
2024-04-14 14:08:38 -0300

    changes in .gitignore, ll_project/urls.py, requirements.txt, and deleted requirements_remote.txt

--- ll_project/urls.py
+++ ll_project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 """
 URL configuration for ll_project project.
 
@@ -22,3 +24,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', include('blog_app.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
