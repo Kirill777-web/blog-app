cd4df5fc184832353b4ea0637961776db33d6e52
Kirill777-web <ki.lazutin@gmail.com>
2024-04-15 17:30:15 -0300

    added debug print statements to wsgi.py

--- ll_project/wsgi.py
+++ ll_project/wsgi.py
@@ -7,10 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
+# ll_project/wsgi.py
 
 from django.core.wsgi import get_wsgi_application
+import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+logger.debug('Starting up...')
+logger.debug(f'Current WORKDIR: {os.getcwd()}')
+logger.debug(f'PYTHONPATH: {os.getenv("PYTHONPATH")}')
+logger.debug(f'WSGI file location: {__file__}')
+logger.debug(f'DJANGO_SETTINGS_MODULE: {os.getenv("DJANGO_SETTINGS_MODULE")}')
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'll_project.settings')
-
 application = get_wsgi_application()
