e6c236063686f88e93e7198cc48d0754f6ec56bd
Kirill777-web <ki.lazutin@gmail.com>
2024-04-16 17:51:15 -0300

    debugging

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         default=os.environ.get('DATABASE_URL')
     )
 }
-
+print("Database URL:", os.environ.get('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
